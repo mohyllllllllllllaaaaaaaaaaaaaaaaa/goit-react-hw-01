388e2ae63c3d78c504406bfd64f30a8a7bbc8335
natalia mohyla <mogilesha81@gmail.com>
2025-03-22 13:08:57 +0100

    fix

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -6,7 +6,8 @@ const FriendListItem = ({avatar, name, isOnline }) =>{
             <img src={avatar} alt="Avatar" width="48" />
             <p className={css.friendName}>{name}</p>
             <p className={clsx(css.status, isOnline ? css.online : css.offline)}>
-                {isOnline ? 'Online' : 'Offline'}
+            {isOnline && 'Online'}
+            {!isOnline && 'Offline'}
             </p>
         </div>
     );
